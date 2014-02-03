17a94e9bb9c378b17abf1562fb2418c1b6de74f3
Denise Li <deniseli1231@gmail.com>
2014-02-03 13:43:46 -0600

    Hello, Nathan

--- Nathan.py
+++ Nathan.py
@@ -1,12 +1,5 @@
-# This is a python script to simulate a conversation with Nathan Handler.  It
-# does not pass the Turing test, but then, neither does the real Nathan, so that
-# limitation exists purely for the sake of realism, and definitely has nothing
-# to do with my ability to make a bot that can pass the Turing test.
-#
-# Denise Li
-# January 2014
-#
-# Nathan.py
+# Hello, Nathan!  How did you find this already?
+# We've been expecting you. ;)
 
 import nltk
 import random
